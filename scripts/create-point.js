439cc29edb9c0746fb689c5e1196947c41f87d69
klepa <pedro.klepa@gmail.com>
2020-06-03 13:42:42 -0300

    Add item selection to the form and handles each selection

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,14 +1,4 @@
-document
-    .querySelector("select[name=uf]")
-    .addEventListener("change", getCities);
-
-
-populateUFs()
-
-
-
-
-// Dinamically fetches brazilian states from IBGE's API and populate the UF Select in the form
+// Dynamically fetches brazilian states from IBGE's API and populate the UF Select in the form
 function populateUFs(){
     const ufSelect = document.querySelector('select[name=uf]');
 
@@ -37,14 +27,67 @@ function getCities(event){
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios
     `;
 
+    citySelect.innerHTML = '<option value="">Selecione a cidade</option>';
+    citySelect.disabled = true;
+
     fetch(url)
         .then( res => res.json() )
         .then( cities => {
             for( const city of cities ) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
             }
 
             citySelect.disabled = false;
         } 
     );
 }
+
+// As the item is selected, updates the global selectedItems array and the hidden input for future reference
+function handleSelectedItem(event){
+    const itemLi = event.target;
+    const itemId = itemLi.dataset.id;
+
+    itemLi.classList.toggle("selected");
+
+
+    // Look through global variable selectedItems for the current itemId
+    const alreadySelected = selectedItems.findIndex( item => item == itemId );
+
+    if (alreadySelected != -1) {
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId;
+            return itemIsDifferent
+        });
+
+        selectedItems = filteredItems;
+    } else {
+        selectedItems.push(itemId);
+    }
+
+    collectedItems.value = selectedItems;
+}
+
+
+
+
+// ..:: Main  ::..
+
+// - Event listeners
+// Cities Select
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities);
+
+// Items grid
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+for( const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem);
+}
+
+// - Global variables
+const collectedItems = document.querySelector("input[name=items]");
+let selectedItems = [];
+
+// Function Calls
+populateUFs()
+
